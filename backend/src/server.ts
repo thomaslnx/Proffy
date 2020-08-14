11a434e8d10b097bcd5cb97eb966de31452c9471
thomaslnx <marcos.msilva10@gmail.com>
2020-08-14 17:37:33 -0300

    Proffy tutorial finished

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes'
+
+const app = express();
+
+const port = process.env.PORT || 3333;
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+app.listen(port);
+console.log(`Servidor on a porta: ${port}`);
